7cc349ea122c50d6268a0eb7252a6f978423ce0f
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-22 04:29:14 -0400

    Updated offers model

--- owner_admin/admin.py
+++ owner_admin/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from owner_admin.models import Offers
 
-# Register your models here.
+
+class OffersAdmin(admin.ModelAdmin):
+    list_display = ('offer', 'description', 'date_added', 'from_date', 'to_date', 'image', 'price')
+
+
+admin.site.register(Offers)

04328e6662e81a7821221f922a2879bc2aaa3722
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-23 00:10:49 -0400

    Added features to add, delete and edit offers

--- flyboredome/urls.py
+++ flyboredome/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('testimonials/', include('testimonials.urls')),
-    path('owner-admin/', include('owner_admin.urls')),
+    path('owner/', include('owner_admin.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- owner_admin/views.py
+++ owner_admin/views.py
@@ -2,15 +2,43 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 
-from testimonials.models import Testimonial, Photos
+from testimonials.models import Testimonial
+from owner_admin.models import Offers
+from owner_admin.forms import AddOrEditOfferForm
 
 
 def index(request):
+    latest_offers = Offers.objects.order_by('-date_added')
     latest_testimonials = Testimonial.objects.order_by('-date_added')
-    return render(request, 'owner_admin/index.html', {'latest_testimonials': latest_testimonials})
+    return render(request, 'owner_admin/index.html', {'latest_testimonials': latest_testimonials,
+                                                      'latest_offers': latest_offers})
 
 
 def delete_testimonial(request, testimonial_id):
     testimonial = Testimonial.objects.get(id=testimonial_id)
     testimonial.delete()
     return HttpResponseRedirect(reverse('owner_admin:admin_home'))
+
+
+def delete_offer(request, offer_id):
+    offer = Offers.objects.get(id=offer_id)
+    offer.delete()
+    return HttpResponseRedirect(reverse('owner_admin:admin_home'))
+
+
+def add_or_edit_offer(request, offer_id=None):
+    if offer_id:
+        offer = Offers.objects.get(id=offer_id)
+    else:
+        offer = None
+    if request.method == 'POST':
+        form = AddOrEditOfferForm(request.POST, request.FILES, instance=offer)
+        if form.is_valid():
+            form.save()
+            return redirect('owner_admin:admin_home')
+        else:
+            message = 'Please correct the error below.'
+            return render(request, 'owner_admin/add_or_edit_offer.html', {'form': form, 'message': message})
+    else:
+        form = AddOrEditOfferForm(instance=offer)
+        return render(request, 'owner_admin/add_or_edit_offer.html', {'form': form})

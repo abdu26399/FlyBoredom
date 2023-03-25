dc9b309d5ceb84c3883db5086a1170c013d7692e
Ishita Tiwari <tiwarii@uwindsor.ca>
2023-03-25 16:14:34 -0400

    Fixed integrity error

--- authentication/views.py
+++ authentication/views.py
@@ -5,15 +5,16 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 
-
 # Create your views here
 
 def home(request):
+    # Render the index.html template
     return render(request, "authentication/index.html")
 
 
 def signup(request):
     if request.method == "POST":
+        # Get the form data
         username = request.POST['username']
         firstname = request.POST['firstname']
         lastname = request.POST['lastname']
@@ -26,40 +27,48 @@ def signup(request):
             myuser.first_name = firstname
             myuser.last_name = lastname
 
-            # save in database
+            # Save the user object to the database
             myuser.save()
+
+            # Add a success message and redirect to the signin page
             messages.success(request, "Your account is successfully created.")
             return redirect('signin')
 
         except IntegrityError:
-            return redirect('signup')
+            return redirect('signin')
 
+    # Render the signup.html template
     return render(request, "authentication/signup.html")
 
 
 def signin(request):
-
     if request.method == 'POST':
+        # Get the form data
         username = request.POST['username']
         pwd1 = request.POST['pwd1']
 
+        # Authenticate the user using the entered credentials
         user = authenticate(username=username, password=pwd1)
 
-        #if user typed right credentials which is saved in database then login user, returns None response if user is not authenticated
+        # If the user is authenticated, log them in and redirect to the index page with their name displayed
         if user is not None:
             login(request,user)
             firstname = user.first_name
             return render(request, "authentication/index.html", {'firstname': firstname})
 
-        #credentials not matched
+        # If the user is not authenticated, add an error message and redirect back to the home page
         else:
             messages.error(request, " Incorrect credentials! Please check your Username and Password. Try again")
             return redirect('home')
 
+    # Render the signin.html template
     return render(request, "authentication/signin.html")
 
 
 def signout(request):
+    # Log out the user and add a success message
     logout(request)
     messages.success(request,"You have logged out")
+
+    # Redirect to the home page
     return redirect('home')

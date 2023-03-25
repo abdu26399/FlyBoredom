e3eebefb0a6e3dada4ce745d5d0004e2cac454d5
Dhruv Nair <nair.dhruv@gmail.com>
2023-03-25 19:42:35 -0400

    Added cart and checkout

---
+++ cart/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.view_cart, name='view_cart'),
+    path('add/<int:offer_id>', views.add_to_cart, name='add_to_cart'),
+    path('remove/<int:cart_item_id>', views.remove_from_cart, name='remove_from_cart'),
+    path('checkout', views.checkout, name='checkout'),
+]

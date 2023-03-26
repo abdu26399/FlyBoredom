015772ee863d66497a4317e2882b9ac0545be3d6
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-26 00:27:26 -0400

    Changed date ranges to a single date

--- owner_admin/admin.py
+++ owner_admin/admin.py
@@ -3,8 +3,8 @@ from owner_admin.models import Offers
 
 
 class OffersAdmin(admin.ModelAdmin):
-    list_display = ('offer', 'date_added', 'from_date', 'to_date', 'price', 'spots_available', 'status')
-    search_fields = ('offer', 'description', 'date_added', 'from_date', 'to_date', 'image', 'price')
+    list_display = ('offer', 'date_added', 'date', 'price', 'spots_available', 'status')
+    search_fields = ('offer', 'description', 'date_added', 'date', 'image', 'price')
 
 
 admin.site.register(Offers, OffersAdmin)

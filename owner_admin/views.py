b6b50894670bc2b9f7cf82d13c6641e1ce7dc7e6
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-26 00:46:02 -0400

    Verifying if user is staff

--- owner_admin/views.py
+++ owner_admin/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpResponseRedirect
@@ -7,6 +8,7 @@ from owner_admin.models import Offers
 from owner_admin.forms import AddOrEditOfferForm
 
 
+@user_passes_test(lambda u: u.is_staff, login_url='testimonials:list-testimonials')
 def index(request):
     latest_offers = Offers.objects.order_by('-date_added')
     latest_testimonials = Testimonial.objects.order_by('-date_added')
@@ -14,18 +16,21 @@ def index(request):
                                                       'latest_offers': latest_offers})
 
 
+@user_passes_test(lambda u: u.is_staff, login_url='testimonials:list-testimonials')
 def delete_testimonial(request, testimonial_id):
     testimonial = Testimonial.objects.get(id=testimonial_id)
     testimonial.delete()
     return HttpResponseRedirect(reverse('owner_admin:admin_home'))
 
 
+@user_passes_test(lambda u: u.is_staff, login_url='testimonials:list-testimonials')
 def delete_offer(request, offer_id):
     offer = Offers.objects.get(id=offer_id)
     offer.delete()
     return HttpResponseRedirect(reverse('owner_admin:admin_home'))
 
 
+@user_passes_test(lambda u: u.is_staff, login_url='testimonials:list-testimonials')
 def add_or_edit_offer(request, offer_id=None):
     if offer_id:
         offer = Offers.objects.get(id=offer_id)

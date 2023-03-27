5520b3e238dc526d472ace26c505c335b74f2e4e
Dhruv Nair <nair.dhruv@gmail.com>
2023-03-26 23:20:35 -0400

    Login required to add testimonial

--- testimonials/urls.py
+++ testimonials/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views 
 
 app_name = 'testimonials'
 
 urlpatterns = [
     path('', views.TestimonialListView.as_view(), name="list-testimonials"),
-    path('add/', views.CreateTestimonialView.as_view(), name="add-testimonial"),
+    path('add/', login_required(views.CreateTestimonialView.as_view()), name="add-testimonial"),
 ]
 
